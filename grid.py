5df2c2633b7609905abe7df90a12d151d53543d6
Jules Deleuse <144125393+KryMucus@users.noreply.github.com>
2024-01-22 20:02:28 +0100

    Update grid.py

--- grid.py
+++ grid.py
@@ -1,4 +1,6 @@
 import random
+import shutil
+
 from calculation import get_neighbour_count
 def create_grid(rows, cols):
     return [[random.randint(0, 1) for _ in range(cols)] for _ in range(rows)]
@@ -17,3 +19,15 @@ def update_grid(grid):
                 if count == 3:
                     new_grid[row][col] = 1
     return new_grid
+
+def get_user_grid_size():
+    rows = int(input("Enter number of rows: "))
+    cols = int(input("Enter number of columns: "))
+    return rows, cols
+
+
+
+def get_terminal_size():
+    cols, rows = shutil.get_terminal_size()
+    return rows, cols  # Note: get_terminal_size returns (columns, rows)
+
